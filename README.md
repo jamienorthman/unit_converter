d2e917b04231f8fb657d73cd2246846381575683
Jamienorthman <mikoann88@gmail.com>
2023-06-28 17:48:19 +0200

    removed html for bottom half, refactored js referring only to array, updated readme

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a SOLO project from Scrimba.com
+
+It uses a button-click-eventListener to produce metric and imperial units of measurement based on the user's input.
+
+Once working, the goal was to then do some function-refactoring and make the Javascript more dynamic, using less HTML. Now more information can be added to the units array without breaking the program.
--- app.js
+++ app.js
@@ -1,5 +1,34 @@
+const convertBtn = document.getElementById("convert-btn")
+const container = document.getElementById("bottom-part")
+let inputNum = document.getElementById("input-number")
+
+//  creating the HTML elements for displaying the sections for the units
+//  all in JS, making use of the units array
+//  (with the units array being the "single source of truth", SSOT)
+function renderHTML(title, pName) {
+    let unitDiv = document.createElement("div")
+    unitDiv.classList.add("unit-type")
+
+    let titleEl = document.createElement("p")
+    titleEl.innerHTML = title
+    titleEl.classList.add("measurements")
+
+    let results = document.createElement("p")
+    // Give each freshly created HTML element the correct id,
+    // so they can be identified and accessed later on.
+    results.setAttribute("id", pName)
+    results.classList.add("results")
+
+    container.append(unitDiv)
+    unitDiv.append(titleEl)
+    unitDiv.append(results)
+}
+
+// adding titles, such as "Length (Meter/Feet)", to the units array
+// SSOT:
 const units = [
     {
+        title: "Length (Meter/Feet)",
         metric: "meters",
         imperial: "feet",
         conversionFactor: 3.281,
@@ -7,6 +36,7 @@ const units = [
     },
     
     {
+        title: "Volume (Liters/Gallons)",
         metric: "liters",
         imperial: "gallons",
         conversionFactor: 0.264,
@@ -14,28 +44,36 @@ const units = [
     },
     
     {
+        title: "Mass (Kilograms/Pounds)",
         metric: "kilos",
         imperial: "pounds",
         conversionFactor: 2.204,
         resultsHTML: "mass-results"
     }
 ]
-
-const convertBtn = document.getElementById("convert-btn")
-const inputField = document.getElementById("input-div")
-
-let inputNum = document.getElementById("input-number")
+// rendered html for units and this loop prodcues 3 of them
+// or whatever the amount in the array is
+for (let i = 0; i < units.length; i++) {
+    let pName = units[i].resultsHTML
+    let title = units[i].title
+    renderHTML(title, pName)
+}
+   
 
 convertBtn.addEventListener("click", function() {
+    
     for (let i = 0; i < units.length; i++) {
         let convertMetric = inputNum.value * units[i].conversionFactor
         let convertImp = inputNum.value / units[i].conversionFactor
+        // When the button is clicked, use 'document.getElementById(...)' inside the loop,
+        // to access the right elements for each entry and set their correct values
+        let paragraphEl = document.getElementById(units[i].resultsHTML)
 
-        let paragraphElement = document.getElementById(units[i].resultsHTML)
-
-        paragraphElement.innerHTML = `
-                ${inputNum.value} ${units[i].metric} = ${convertMetric.toFixed(3)} ${units[i].imperial} | 
-                ${inputNum.value} ${units[i].imperial} = ${convertImp.toFixed(3)} ${units[i].metric} 
+        paragraphEl.innerHTML = `
+                ${inputNum.value} ${units[i].metric} = 
+                ${convertMetric.toFixed(3)} ${units[i].imperial} | 
+                ${inputNum.value} ${units[i].imperial} = 
+                ${convertImp.toFixed(3)} ${units[i].metric} 
         `
     }
 })
